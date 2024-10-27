f26f7355777cb826bbae113abcfdb16dbae932ab
1234Chinonso <chinonsotasieobi@hotmail.com>
2024-10-27 18:50:05 +0100

    Done

---
+++ dt.py
@@ -0,0 +1,17 @@
+#Data Types
+a = "totalBombs"
+print("Type of a =", type(a))
+
+b = 300
+print("Type of b =", type(b))
+
+c = 2.5
+print("Type of c =", type(c))
+
+d = True
+print("Type of d =", type(d))
+
+#Typecasting
+c = int(c)
+print("Value of c is", c)
+print("Data type of c", type(c))
